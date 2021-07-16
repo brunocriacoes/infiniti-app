c7f19bf65e16723b2118e7888de56342d30deca4
brunocriacoes <br.rafael@outlook.com>
2021-07-16 14:18:59 -0300

    fix

--- assets/js/component/login.js
+++ assets/js/component/login.js
@@ -13,11 +13,11 @@ export default {
                     <form action="javascript:void(0)" name="form_login" onsubmit="logar()" >
                         <img src="./assets/logo/logo-box-white.png">
                         <label for="">Localizador</label>
-                        <input type="text" name="localizador" value="BRUNO_TESTE" required>                
+                        <input type="text" name="localizador" autocomplete="" value="" required>                
                         <label for="">Nome Usuário</label>
-                        <input type="text"  name="login"value="BRUNO" required>                
+                        <input type="text"  name="login" autocomplete="" value="" required>                
                         <label for="">Senha</label>
-                        <input type="password" name="password" value="BRUNO" required>
+                        <input type="password" name="password" autocomplete="" value="" required>
                         <button type="submit">ENTRAR</button>
                         <div class="error" hidden>Usuário ou Senha errados</div>
                     </form>
